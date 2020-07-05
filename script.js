4374233925cfd8321f0008ecb3ddbaf9ce77e52f
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-07-05 18:03:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+console.log("Cześć");
+
+
+//getting elements from DOM
+let movieButtons = Array.from(document.querySelectorAll(".movie__button"));
+let galleries = Array.from(document.querySelectorAll(".movie__gallery")); //można inaczej też zapisać do array
+let descriptions = Array.from(document.querySelectorAll(".movie__description"));
+
+
+//condition for sidemenu visibility
+window.addEventListener("scroll", () => {
+    let scrollPosition = window.pageYOffset;
+    let sideMenu = document.querySelector(".popup");
+    if (scrollPosition > 400) {
+        sideMenu.classList.add("popup__visible")
+    } else {
+        sideMenu.classList.remove("popup__visible")
+    }
+})
+
+//changing of button's content
+function innerChange(){
+    if (movieButton.innerHTML === "See cast") {
+    movieButton.innerHTML = "Back to movie";
+} else {
+    movieButton.innerHTML = "See cast";
+}
+}
+//functions for adding and removing object's visibility
+//if construction instead of toggle - just for practicing
+function changeGalleryDisplay(){
+    if (gallery.classList.contains("movie__display-JS")) {
+        gallery.classList.remove("movie__display-JS");
+    } else {
+        gallery.classList.add("movie__display-JS");
+    }
+}
+
+function changeDescriptionDisplay(){
+    if (description.classList.contains("movie__display-JS")) {
+        description.classList.remove("movie__display-JS");
+    } else {
+        description.classList.add("movie__display-JS");
+    }
+}
+
+/*adding interactions to gallery and description which clicked button belongs to*/
+movieButtons.forEach(element => {
+    element.addEventListener("click", (e) => {
+        movieButton = e.target;
+        index = movieButtons.indexOf(movieButton);
+        gallery = galleries[index];
+        description = descriptions[index];
+        innerChange();
+        changeGalleryDisplay();
+        changeDescriptionDisplay();
+    })
+});
+
+
+
+
+
+
+
+
